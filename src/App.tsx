af7ec455e845369fa6a38f9c3be88ad54df22bbf
Elias <elias.rivera.ico@gmail.com>
2025-02-15 19:46:28 -0600

    update: Se añade el archivo con los items del menu. Se añade el archivo con los types del proyecto. Se establece conexion entre la App y los items

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
+import { menuItems } from "./data/db"
+
 function App() {
+  console.log(menuItems)
   return (
     <>
-    <h1 className="text-3xl">Hola Mundo</h1>
-    <p className="text-4xl">Esto es un párrafo de muestra para probar estilos de Tailwind CSS</p>
+      <header className="bg-cyan-200 py-5">
+        <h1 className="text-center text-3xl">Calculadora de propinas y consumo</h1>
+      </header>
     </>
   )
 }
