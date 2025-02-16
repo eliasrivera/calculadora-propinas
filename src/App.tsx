22f45cd3dddd2ced3f0cd48baf6749a88d92ae36
Elias <elias.rivera.ico@gmail.com>
2025-02-15 18:39:26 -0600

    ci: Se agrega la configuracion para utilizar el framework Tailwind CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 function App() {
   return (
     <>
-    <h1>Hola Mundo</h1> 
+    <h1 className="text-3xl">Hola Mundo</h1>
+    <p className="text-4xl">Esto es un párrafo de muestra para probar estilos de Tailwind CSS</p>
     </>
   )
 }
