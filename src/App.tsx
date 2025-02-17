54bb9f8319cd3cf0a3bc0e607ff848d8affce629
Elias <elias.rivera.ico@gmail.com>
2025-02-16 19:33:14 -0600

    update: Se agrega el componente MenuItem. Se agrega la estructura base para visualizar el menú. Se aplica un type al arreglo que contiene la info de los items en db

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
+import MenuItem from "./components/MenuItem"
 import { menuItems } from "./data/db"
 
 function App() {
-  console.log(menuItems)
   return (
     <>
-      <header className="bg-cyan-200 py-5">
-        <h1 className="text-center text-3xl">Calculadora de propinas y consumo</h1>
+      <header className="bg-cyan-700 py-5">
+        <h1 className="text-center text-3xl text-white">Calculadora de propinas y consumo</h1>
       </header>
+      <main className=" max-w-7xl mx-auto py-20 grid md:grid-cols-2">
+        <div className="p-5">
+          <h2 className="text-3xl">Menú</h2>
+          <div className="space-y-3 mt-10">
+            {menuItems.map(item => (
+              <MenuItem
+                key={item.id}
+                item={item}
+              />
+            ))}
+          </div>
+        </div>
+        <div>
+          <h2>Consumo</h2>
+        </div>
+      </main>
     </>
   )
 }
---
+++ src/components/MenuItem.tsx
@@ -0,0 +1,16 @@
+import type { MenuItem } from "../types"
+
+type MenuItemProps = {
+    item: MenuItem
+}
+
+export default function MenuItem({ item }: MenuItemProps) {
+    return (
+        <button
+            className="border-2 border-teal-700 hover:bg-indigo-300 w-full p-3 flex justify-between"
+        >
+            <p>{item.name}</p>
+            <p className="font-black">${item.price}</p>
+        </button>
+    )
+}
